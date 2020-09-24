2ba663617bba7f99d498cf3e1b2d2fda0f4a72ba
hvanli <hvanli894@gmail.com>
2020-09-24 18:56:24 -0500

    TestStash file modified

--- src/com/stash/TestStash.java
+++ src/com/stash/TestStash.java
@@ -2,4 +2,9 @@ package com.stash;
 
 public class TestStash {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Modification of our code");
+		System.out.println("Hello Wolrd");
+	}
 }
